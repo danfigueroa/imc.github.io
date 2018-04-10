146158c8e608f5d6d047f05e2b47bb1f63747666
Daniel Figueroa <danielmfigueroa@gmail.com>
2018-04-10 20:40:20 -0300

    Alterações no background, alertas do js e estrutura do html

--- js/imc.js
+++ js/imc.js
@@ -6,21 +6,21 @@ function calcula_imc() {
     calculo = Math.round(calculo*100)/100;
 
     if(calculo<18.5) {
-      alert("Você está muito abaixo do seu peso ideal com esse indice: " + calculo);
+      alert("Você está muito abaixo do seu peso ideal com o IMC: " + calculo);
     }
 
     else if(calculo>=18.5 && calculo<24.9) {
-      alert("Você está dentro do limite recomendado com esse indice: " + calculo);
+      alert("Você está dentro do limite recomendado com o IMC: " + calculo);
     }
 
     else if(calculo>=25 && calculo<29.9) {
-      alert("Você está acima do peso recomendado com esse indice: " + calculo);
+      alert("Você está acima do peso recomendado com o IMC: " + calculo);
     }
     
     else if(calculo>=30 && calculo<39.9) {
-      alert("Você está com obesidade com esse indice: " + calculo);
+      alert("Você está com obesidade com o IMC: " + calculo);
     }
     
     else if (calculo>40)
-      alert("Você estácom obesidade grave com esse indice: " + calculo);
+      alert("Você está com obesidade grave com o IMC: " + calculo);
     }
